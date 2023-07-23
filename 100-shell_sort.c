0b594bccb6be3dddd2aadb4673764d6d96cb7bca
AhmedElSaeedTalat <ahmedelsaeed105@gmail.com>
2023-07-23 18:27:02 +0000

    shell sort

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -7,16 +7,18 @@
   */
 void shell_sort(int *array, size_t size)
 {
-	int gap, j, i, temp;
-	int len = size;
+	size_t gap, j;
+	int temp, i;
 
+	if (array == NULL)
+		return;
 	gap = 1;
-	while (gap < len / 3)
+	while (gap < size / 3)
 		gap = gap * 3 + 1;
 
 	for (gap = gap; gap >= 1; gap = (gap - 1) / 3)
 	{
-		for (j = gap; j < len; j++)
+		for (j = gap; j < size; j++)
 		{
 			temp = array[j];
 			for (i = j - gap; array[i] > temp && i >= 0; i = i - gap)
